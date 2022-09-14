a1bf35f8e8dcd1a49c0e5f6420aaf53e5c1170ee
Camilo López <camiloj.lopez79@gmail.com>
2022-09-14 16:24:06 -0300

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Camilo López on 14-09-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
